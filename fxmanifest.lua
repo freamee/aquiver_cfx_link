da131ec3e7b380a3f31d082b841d56d9f0a23995
Krisztián S. <sutyakkrisztian999@hotmail.com>
2026-08-26 12:41:51 +0200

    fixed missing links

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,6 +9,25 @@ provide {
     'aquiver'
 }
 
+client_scripts {
+    "links/inventories/ox_inventory/client.lua",
+    "links/inventories/quasar_inventory/client.lua",
+    "links/inventories/one_inventory/client.lua",
+
+    "client_exports.lua"
+}
+
+server_scripts {
+    "links/frameworks/esx/server.lua",
+    "links/frameworks/qbcore/server.lua",
+    "links/frameworks/qbox/server.lua",
+
+    "links/inventories/ox_inventory/server.lua",
+    "links/inventories/quasar_inventory/server.lua",
+
+    "server/exports.lua"
+}
+
 shared_scripts {
     '@ox_lib/init.lua',
 }

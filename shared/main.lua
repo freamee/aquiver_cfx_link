b9c11672b9a7e76c4028cc551ab7646ef8fa7d83
Krisztián S. <sutyakkrisztian999@hotmail.com>
2026-09-01 15:22:31 +0200

    push isShowcaseServer

--- shared/main.lua
+++ shared/main.lua
@@ -4,6 +4,12 @@ local Log = require("shared.Log")
 
 CfxShared = {}
 
+CfxShared.Debug = false
+
 CfxShared.lerp = lerp
 CfxShared.Tick = Tick
 CfxShared.Log = Log
+
+CfxShared.isShowcaseServer = function()
+    return GetConvarBool("aquiver_showcase_server", false)
+end

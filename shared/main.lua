c280ccc024c0935779753452b286c7b89cf55812
Krisztián S. <sutyakkrisztian999@hotmail.com>
2026-09-01 20:38:07 +0200

    smoothnumber

--- shared/main.lua
+++ shared/main.lua
@@ -1,6 +1,7 @@
 local lerp = require("shared.lerp")
 local Tick = require("shared.Tick")
 local Log = require("shared.Log")
+local SmoothNumber = require("shared.SmoothNumber")
 
 CfxShared = {}
 
@@ -9,6 +10,8 @@ CfxShared.Debug = false
 CfxShared.lerp = lerp
 CfxShared.Tick = Tick
 CfxShared.Log = Log
+
+CfxShared.SmoothNumber = SmoothNumber
 
 CfxShared.isShowcaseServer = function()
     return GetConvarBool("aquiver_showcase_server", false)
